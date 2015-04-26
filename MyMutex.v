21adb9ae32a180f4ba2a1a4403034325ae9a6ea1
Wang Xiayang <wangxiayang.fdt@gmail.com>
2015-04-26 22:00:21 +0800

    add wwsafe theorem

--- MyMutex.v
+++ MyMutex.v
@@ -19,7 +19,7 @@ Inductive Mode :=
 Definition MyLock (mode : Mode) (thread : Thread) (mutex : MyMutex) : bool * MyMutex :=
 	match mode with
 	| Write => if negb (held mutex) then (true, (mkMutex true 0 [thread])) else (false, mutex)
-	| Read => if eq_nat_dec (rn mutex) 0 then
+	| Read => if beq_nat (rn mutex) 0 then
 				if held mutex then (false, mutex) else (true, mkMutex true 1 [thread])
 			else (true, mkMutex true (1 + rn mutex) (thread :: holder mutex))
 	end.
@@ -27,13 +27,13 @@ Definition MyLock (mode : Mode) (thread : Thread) (mutex : MyMutex) : bool * MyM
 Fixpoint inlist (a : nat) (l : list nat) : bool :=
 	match l with
 	| [] => false
-	| a' :: l' => if eq_nat_dec a a' then true else inlist a l'
+	| a' :: l' => if beq_nat a a' then true else inlist a l'
 	end.
 
 Fixpoint rmlist (a : nat) (l : list nat) : list nat :=
 	match l with
 	| [] => l
-	| a' :: l' => if eq_nat_dec a' a then l' else a' :: (rmlist a l')
+	| a' :: l' => if beq_nat a' a then l' else a' :: (rmlist a l')
 	end.
 
 Definition MyUnlock (thread : Thread) (mutex : MyMutex) : MyMutex :=
@@ -47,4 +47,25 @@ Definition MyUnlock (thread : Thread) (mutex : MyMutex) : MyMutex :=
 	| false => mutex
 	end. 
 
+Definition GetMutexResult (mode : Mode) (thread : Thread) (mutex : MyMutex) : bool
+:= let (f, _) := MyLock mode thread mutex in f.
+
 End Mutex.
+
+Lemma lock_safe : forall mode thread mutex, (held mutex) = true -> (beq_nat (rn mutex) 0) = true -> (GetMutexResult mode thread mutex) = false.
+Proof.
+intros.
+unfold GetMutexResult.
+unfold MyLock.
+destruct mode.
+ rewrite H.
+ simpl.
+ trivial.
+
+ rewrite H0.
+ rewrite H.
+ trivial.
+Qed.
+
+Theorem wwsafe : forall (m m' m'': MyMutex) (tid1 tid2 : Thread),
+	(true, m') = MyLock Write tid1 m -> (false, m'') = MyLock Write tid2 m'.
